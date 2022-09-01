cede6a77c79cfb66bc055dbdaeca2a976906b19b
André Marcílio <andrehmarcilio@gmail.com>
2022-09-01 12:25:27 -0300

    Revert "projeto inicial"
    This reverts commit bd61ff1baa3de0530d8dcfc0a47f1965a504ac4c.

--- android/app/src/main/kotlin/com/example/kanban_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.kanban_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/domain/irepository/kanban_irepository.dart
+++
@@ -1,8 +0,0 @@
-
-import '../entity/kanban.dart';
-
-abstract class IRepository {
-  List<KanBan> getKanbans();
-  Future<void> updateKanban(KanBan kanban);
-  Future<void> createKanban(KanBan kanban);
-}
